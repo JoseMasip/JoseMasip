0ded3d6739a09aec2e943c2366f872053c23bd15
JoseMasip <jl.r.masip@gmail.com>
2024-04-07 07:06:04 +0100

    Update Script.js

--- js/Script.js
+++ js/Script.js
@@ -1,52 +1,39 @@
-document.querySelector(".menu-icon").addEventListener("click", function(event) {
-    var dropdown = document.querySelector(".menu-dropdown");
-    dropdown.style.display = dropdown.style.display === "flex" ? "none" : "flex";
-});
+document.addEventListener("DOMContentLoaded", function() {
 
-document.getElementById('toggle').addEventListener('click', function(event) {
-    event.stopPropagation(); // Evita la propagación del evento a otros elementos
-
-    var contenedorTexto = document.getElementById('contenedorTexto');
-    var modelo = document.getElementById('Modelo');
-
-    if (contenedorTexto && modelo) {
-        if (contenedorTexto.style.display === 'none' || contenedorTexto.style.display === '') {
-            contenedorTexto.style.display = 'flex'; // Mostrar el div contenedor de texto
-            modelo.style.width = '100%'; // Centrar el modelo
-            modelo.style.margin = 'auto'; // Centrar horizontalmente el modelo
-        } else {
-            contenedorTexto.style.display = 'none'; // Ocultar el div contenedor de texto
-            modelo.style.width = '80%'; // Descentrar el modelo
-            modelo.style.margin = 'initial'; // Reiniciar el margen
-        }
-    }
-
-    var modelo = document.getElementById('Modelo');
-    if (modelo) {
-        if (modelo.style.width === '80%') {
-            modelo.style.width = '100%'; // Centrar el modelo
-            modelo.style.margin = 'auto'; // Centrar horizontalmente el modelo
-        } else {
-            modelo.style.width = '80%'; // Descentrar el modelo
-            modelo.style.margin = 'initial'; // Reiniciar el margen
-        }
-    }
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    var cambioTrigger = document.getElementById('cambio-trigger');
-    var modeloViewer = document.querySelector('#Modelo model-viewer');
-
-    cambioTrigger.addEventListener('click', function() {
-        // Obtener el modelo-viewer actual
-        var currentModel = modeloViewer;
-        // Verificar qué modelo se está mostrando actualmente
-        if (currentModel.getAttribute('src') === 'media/glb/scenaVoxel.glb') {
-            // Si se está mostrando sceneVoxel.glb, cambiar a contenedor.glb
-            currentModel.setAttribute('src', 'media/glb/contenedor.glb');
-        } else {
-            // Si se está mostrando contenedor.glb o cualquier otro modelo, cambiar a sceneVoxel.glb
-            currentModel.setAttribute('src', 'media/glb/scenaVoxel.glb');
-        }
+    document.querySelector(".menu-icon").addEventListener("click", function(event) {
+        var dropdown = document.querySelector(".menu-dropdown");
+        dropdown.style.display = dropdown.style.display == "none" ? "flex": "none";
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("cambio-trigger").addEventListener("click", function(event) {
+        window.location.href = 0;
+        // Redirecciona a la página "props.html" al hacer clic en el elemento con id "cambio-trigger"
+        window.location.href = "https://josemasip.github.io/JoseMasip/";
+    });
+});
+
+/*
+
+<a href="sections/props.html">Props</a>
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Rutas de los modelos GLB
+    var modelos = [];
+   
+    // Agrega un event listener al enlace
+    document.getElementById("cambio-trigger").addEventListener("click", function(event) {
+        // Obtén el elemento model-viewer
+        var modeloViewer = document.querySelector("model-viewer");
+
+        // Cambia la URL del modelo GLB utilizando el contador
+        modeloViewer.src = modelos[contadorModelo];
+
+        // Incrementa el contador y vuelve al principio si supera el número de modelos
+        contadorModelo = (contadorModelo + 1) % modelos.length;
+
+        // Evita que el enlace haga su acción predeterminada (navegar a otra página)
+        event.preventDefault();
+    });
+});*/
