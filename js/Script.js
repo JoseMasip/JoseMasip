a6e6b68c3f9f794af7d52a3c393d39be1b8f4be5
JoseMasip <166128717+JoseMasip@users.noreply.github.com>
2024-04-07 06:32:09 +0100

    Update Script.js

---
+++ js/Script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.querySelector(".menu-icon").addEventListener("click", function(event) {
+        var dropdown = document.querySelector(".menu-dropdown");
+        dropdown.style.display = dropdown.style.display == "none" ? "flex": "none";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("cambio-trigger").addEventListener("click", function(event) {
+        window.location.href = 0;
+        // Redirecciona a la página "props.html" al hacer clic en el elemento con id "cambio-trigger"
+        window.location.href = "http://127.0.0.1:5500/index.html";
+    });
+});
+
+/*
+
+<a href="sections/props.html">Props</a>
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Rutas de los modelos GLB
+    var modelos = [];
+   
+    // Agrega un event listener al enlace
+    document.getElementById("cambio-trigger").addEventListener("click", function(event) {
+        // Obtén el elemento model-viewer
+        var modeloViewer = document.querySelector("model-viewer");
+
+        // Cambia la URL del modelo GLB utilizando el contador
+        modeloViewer.src = modelos[contadorModelo];
+
+        // Incrementa el contador y vuelve al principio si supera el número de modelos
+        contadorModelo = (contadorModelo + 1) % modelos.length;
+
+        // Evita que el enlace haga su acción predeterminada (navegar a otra página)
+        event.preventDefault();
+    });
+});*/
